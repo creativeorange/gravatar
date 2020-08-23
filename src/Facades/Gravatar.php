4848baf46d7bb1b7c78741bff0f7d90c38018027
Tom Witkowski <dev.gummibeer@gmail.com>
2020-08-23 13:44:48 +0200

    add see line

--- src/Facades/Gravatar.php
+++ src/Facades/Gravatar.php
@@ -8,16 +8,18 @@ use Illuminate\Support\Facades\Facade;
  * @method static \Creativeorange\Gravatar\Gravatar fallback(string $fallback)
  * @method static string get(string $email, string $configGroup = 'default')
  * @method static bool exists(string $email)
+ * 
+ * @see \Creativeorange\Gravatar\Gravatar
  */
 class Gravatar extends Facade
 {
-    /**
-     * Get the registered name of the component.
-     *
-     * @return string
-     */
-    protected static function getFacadeAccessor() 
-    { 
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor()
+    {
         return 'gravatar';
     }
 }
