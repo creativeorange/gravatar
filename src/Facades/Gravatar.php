77aa65f91fa179c5b463a9cb6f6e8e2673b50dce
Tom Witkowski <dev.gummibeer@gmail.com>
2020-08-27 14:16:41 +0200

    improve facade with doc-block (#25)
    * improve facade with doc-block
    
    * add see line
    
    * fix config type-hints

--- src/Facades/Gravatar.php
+++ src/Facades/Gravatar.php
@@ -1,14 +1,25 @@
-<?php namespace Creativeorange\Gravatar\Facades;
+<?php
+
+namespace Creativeorange\Gravatar\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-class Gravatar extends Facade {
-
+/**
+ * @method static \Creativeorange\Gravatar\Gravatar fallback(string $fallback)
+ * @method static string get(string $email, string|array|null $configGroup = 'default')
+ * @method static bool exists(string $email)
+ *
+ * @see \Creativeorange\Gravatar\Gravatar
+ */
+class Gravatar extends Facade
+{
 	/**
 	 * Get the registered name of the component.
 	 *
 	 * @return string
 	 */
-	protected static function getFacadeAccessor() { return 'gravatar'; }
-
+	protected static function getFacadeAccessor()
+    {
+        return 'gravatar';
+    }
 }
